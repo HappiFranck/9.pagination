37a8022e9ad145f0a650dd32f2b42b4d8579510a
Grafikart <contact@grafikart.fr>
2017-08-03 17:00:08 +0200

    Added router

---
+++ src/Framework/Router/Route.php
@@ -0,0 +1,57 @@
+<?php
+namespace Framework\Router;
+
+/**
+ * Class Route
+ * Represent a matched route
+ */
+class Route
+{
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var callable
+     */
+    private $callback;
+
+    /**
+     * @var array
+     */
+    private $parameters;
+
+    public function __construct(string $name, callable $callback, array $parameters)
+    {
+        $this->name = $name;
+        $this->callback = $callback;
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return callable
+     */
+    public function getCallback(): callable
+    {
+        return $this->callback;
+    }
+
+    /**
+     * Retrieve the URL parameters
+     * @return string[]
+     */
+    public function getParams(): array
+    {
+        return $this->parameters;
+    }
+}
