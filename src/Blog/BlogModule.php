7cc47c91c0d028273e188d0b16ff58bbdcc91660
Grafikart <contact@grafikart.fr>
2017-08-04 15:07:30 +0200

    Ajout du renderer

--- src/Blog/BlogModule.php
+++ src/Blog/BlogModule.php
@@ -1,26 +1,32 @@
 <?php
 namespace App\Blog;
 
+use Framework\Renderer;
 use Framework\Router;
-use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class BlogModule
 {
 
-    public function __construct(Router $router)
+    private $renderer;
+
+    public function __construct(Router $router, Renderer $renderer)
     {
+        $this->renderer = $renderer;
+        $this->renderer->addPath('blog', __DIR__ . '/views');
         $router->get('/blog', [$this, 'index'], 'blog.index');
-        $router->get('/blog/{slug:[a-z\-]+}', [$this, 'show'], 'blog.show');
+        $router->get('/blog/{slug:[a-z\-0-9]+}', [$this, 'show'], 'blog.show');
     }
 
     public function index(Request $request): string
     {
-        return '<h1>Bienvenue sur le blog</h1>';
+        return $this->renderer->render('@blog/index');
     }
 
     public function show(Request $request): string
     {
-        return '<h1>Bienvenue sur l\'article ' . $request->getAttribute('slug') . '</h1>';
+        return $this->renderer->render('@blog/show', [
+            'slug' => $request->getAttribute('slug')
+        ]);
     }
 }
---
+++ views/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= isset($title) ? $title : 'Mon site'; ?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"
+          integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+    <style>
+        body {
+            padding-top: 5rem;
+        }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
+    <a class="navbar-brand" href="#">Title</a>
+    <ul class="nav navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+        </li>
+    </ul>
+</nav>
+
+<div class="container">
